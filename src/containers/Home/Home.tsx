d8b93e180058d3644098f4119949c6f10f359b60
Kate YeEum Lee <yeeumm@gmail.com>
2020-05-17 13:55:28 -0400

    feat: add title and list on home

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Typography, Box } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 import { FORM_URL, FORM } from '../../constants';
 
 export const Home = () => {
     return (
-        <Link to={FORM_URL}>{FORM}</Link>
+        <Box p={6}>
+            <Typography variant='h4'>React Materials</Typography>
+            <ul>
+                <li>
+                    <Link to={FORM_URL}>{FORM}</Link>
+                </li>
+            </ul>
+        </Box>
     );
 }

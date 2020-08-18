c86641187b4298e7ade42629591204ff0ddf58c0
Kate YeEum Lee <yeeumm@gmail.com>
2020-08-18 00:01:07 -0400

    feat: add stopwatch route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Form, Home } from './containers'
-import { DEFAULT_URL, FORM_URL } from './constants';
+import { Form, Home, Stopwatch } from './containers'
+import { DEFAULT_URL, FORM_URL, STOPWATCH_URL } from './constants';
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <Switch>
         <Route path={DEFAULT_URL} exact><Home /></Route>
         <Route path={FORM_URL}><Form /></Route>
+        <Route path={STOPWATCH_URL}><Stopwatch /></Route>
       </Switch>
     </Router>
   );

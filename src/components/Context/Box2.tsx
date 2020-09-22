ac32cb95163142f8719c92bcb590f31236f2eccb
Kate YeEum Lee <yeeumm@gmail.com>
2020-09-21 19:13:35 -0700

    feat: pass updating userdata

--- src/components/Context/Box2.tsx
+++ src/components/Context/Box2.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Box, withStyles, WithStyles, Typography } from '@material-ui/core'
 import { Box3 } from '../'
+import { UserContext } from '../../containers'
 
 const styles = {
     root: {
@@ -13,10 +14,12 @@ const styles = {
 
 const Box2Base = (props: WithStyles<typeof styles>) => {
     const { classes } = props
+    const userData = useContext(UserContext)
 
     return (
         <Box className={classes.root}>
             <Typography variant='body1' color='primary'>Box2</Typography>
+            <Typography variant='body2' color='textPrimary'>{userData.userName}</Typography>
             <Box3 />
         </Box>
     )
--- src/containers/Context/Context.tsx
+++ src/containers/Context/Context.tsx
@@ -1,24 +1,26 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 import { Box, Button } from '@material-ui/core'
 import { DEFAULT_URL } from '../../constants'
 import { Link } from 'react-router-dom'
 import { Box1 } from '../../components'
 
 interface IUserData {
-    userId: string
     userName: string
+    setUserName: React.Dispatch<React.SetStateAction<string>>
 }
 
 const userData: IUserData = {
-    userId: 'c5d86fcc-be17-46f3-ba42-0c8a312908bc',
-    userName: 'Andrew'
+    userName: '',
+    setUserName: () => { }
 }
 
 export const UserContext = createContext(userData)
 
 export const Context = () => {
+    const [userName, setUserName] = useState('Andrew')
+
     return (
-        <UserContext.Provider value={userData}>
+        <UserContext.Provider value={{ userName, setUserName }}>
             <Box m={1}>
                 <Link to={DEFAULT_URL}><Button>Back to Home</Button></Link>
             </Box>

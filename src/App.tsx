c062b7d22686860afdd2d0068cf681a54a991133
Kate YeEum Lee <yeeumm@gmail.com>
2022-01-03 23:51:10 -0800

    Undo router changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { BrowserRouter as Switch, Route, HashRouter } from 'react-router-dom'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import { Form, Home, Stopwatch, Context, Todo } from './containers'
 import { DEFAULT_URL, FORM_URL, STOPWATCH_URL, CONTEXT_URL, REDUCER_URL } from './constants';
 
 function App() {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Switch>
         <Route path={DEFAULT_URL} exact><Home /></Route>
         <Route path={FORM_URL}><Form /></Route>
@@ -13,7 +13,7 @@ function App() {
         <Route path={CONTEXT_URL}><Context /></Route>
         <Route path={REDUCER_URL}><Todo /></Route>
       </Switch>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
